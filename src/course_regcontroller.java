3164c17e6d8bce9fe214f9186c0ad53dc8888f90
shreyas38 <54844027+shreyas38@users.noreply.github.com>
2022-04-28 19:09:51 +0530

    Delete src directory

--- src/course_regcontroller.java
+++
@@ -1,9 +0,0 @@
-public class course_regcontroller {
- 
-    
-    static public void register_course(studentmodel stud){
-
-        course_regmodel.reg(stud);
-
-    }
-}
--- src/course_regview.java
+++
@@ -1,3 +0,0 @@
-public class course_regview {
-    
-}
